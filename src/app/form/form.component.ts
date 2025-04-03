3631483cefecedefaf84f7624bfddd5587e9058d
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-04-02 19:29:34 -0600

    template driven form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -4,8 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-form',
   standalone: false,
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
-
+  onSubmit(form: any) {
+    console.log('Form submitted:', form.value);
+  }
 }
